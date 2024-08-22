6892643789eb29188d2284355e1b9f369eeb324a
TC120 <93326855+TC120@users.noreply.github.com>
2024-08-22 17:29:22 +0800

    Create val_test.c

---
+++ test1/test1.5/src/val_test.c
@@ -0,0 +1,65 @@
+/* test how to statement a varable lenght array(val)*/
+
+#include <stdio.h>
+
+
+int val_test(int size1, int size2, int size3)
+{
+    // int size1, size2, size3;
+
+    // printf("please input there int nums \n");
+    // scanf("%d%d%d", &size1, &size2, &size3);
+
+    int array1[size1];
+    int array2[size2][size3];
+
+    int i;
+
+    for(i = 0; i < size1; i++)
+    {
+        *(array1 + i) = i;
+    }
+
+    int j;
+    for(i = 0; i < size2; i++)
+    {
+        for(j = 0; j < size3; j++)
+        {
+            array2[i][j] = i * size3 + j;
+        }
+        
+    }
+
+    for(i = 0; i < size1; i++)
+    {
+        printf("array1[%d] is %d\n" , i, *(array1 + i));
+    }
+
+    for(i = 0; i < size2; i++)
+    {
+        for(j = 0; j < size2; j++)
+        {
+            printf("array2[%d] is %d\n" , i, array2[i][j]);
+        }
+        
+    }
+
+    printf("size of array1 is %zd\n" , sizeof(array1));
+    printf("size of array2 is %zd\n" , sizeof(array2));
+
+    return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
